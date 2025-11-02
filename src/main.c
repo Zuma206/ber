fd5dbbe58b9d7401e5a45358a66af6367f27b6a6
Zuma206 <zuma@zuma.eu.org>
2025-11-02 12:20:34 +0000

    Tested duktape in main

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
+#include <duktape.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-  printf("Hello, World!\n");
+  duk_context *ctx = duk_create_heap_default();
+  duk_eval_string(ctx, "4 * 2");
+  double result = duk_get_number(ctx, -1);
+  printf("Result: %f\n", result);
   return EXIT_SUCCESS;
 }
